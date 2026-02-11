02e118e8fb1be4d6f137ee5752770343b00e3178
Bharti's MAC <apple@MacBook-Pro.local>
2026-02-11 21:18:25 +0530

    assignment1

---
+++ Assignements/Assignement1.ts
@@ -0,0 +1,86 @@
+
+//1. Temperature of a city in degrees Celsius: 25.5
+let temp:number = 25.5;
+console.log ("Temperature of a city in degrees Celsius:"+temp);
+
+//2. placed order : true/false
+let orderPlaced: boolean = true; 
+console.log("Order placed:"+orderPlaced); 
+
+//3. phoner number of the person
+let phoneNumber: number = 123456789; 
+console.log("Phone number is: "+phoneNumber); 
+
+//4. Amount in customer's account
+let acBal: number = 12345; 
+console.log("Account Balance is:"+acBal); 
+
+//5. Person's email address
+let emailAdd:any = "amit@yopmail.com"
+console.log("My email is:"+emailAdd);
+
+//6. Co-ordinates of location 
+let corLoc:number[]= [234.43, -23.4];
+console.log("Cordinates location is:"+corLoc); 
+
+//7. Person's marital status
+let perStatus:boolean = true;
+console.log("Peron's marital status is:"+perStatus);
+
+//8. Person's Occupation 
+let perOccupation:string = '"Software Tester"'
+console.log("Occupation of the person is:"+perOccupation);
+
+
+//9. Person's fav color
+let favColor:string = "Blue";
+console.log("Fav color is:"+favColor); 
+
+
+//10. Current date
+let currentYear: number = new Date().getFullYear();
+console.log("Current year is:"+currentYear); 
+
+//11. Number of followers on social media
+let totalFollowers:number = 123456;
+console.log("Total number of followers are: "+totalFollowers);
+
+//12. Rating of a movie
+let movieRating:number = 8.5;
+console.log("Movie rating is: "+movieRating);
+    
+//13. Person's blood group is
+let bloodGroup:string = "'O+'"
+console.log("Person's blood group is: "+bloodGroup);
+
+// 14.Title of a book: "To Kill a Mockingbird" 
+let bookTitle:string = '"To Kill a Mockingbird"'
+console.log("The book title is: "+bookTitle);
+
+
+//15.Number of employees in a company: 500 
+let empCount: number = 1234
+console.log("Number of emps are: "+empCount); 
+
+
+//16.Time of an event: 2:30 PM 
+let eventTime: any = '2:30 PM';
+console.log("The event timing is: "+eventTime);
+
+//17.Name of a country: "United States" 
+let countryName:string = '"United States"';
+console.log("The country name is: "+countryName);
+
+//18.Person's eye color: "Brown" 
+let eyeColor:string = "Brown";
+console.log("Person's eye color: "+eyeColor); 
+
+//19.Person's birthplace: "New York City"
+let birthPlace: string = '"NY City"'; 
+console.log("Person's birthplace: "+birthPlace); 
+
+//20. Distance between two cities: 200.5
+let disBetCity: number = 200.5
+console.log("Distance is: "+disBetCity);
+
+
