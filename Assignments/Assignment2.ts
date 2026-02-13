4895c29effd57f5e293032dd6a491c3c107e9be2
Bharti's MAC <Newgit.ac@gmail.com>
2026-02-13 08:15:42 +0530

    Assignment 2

---
+++ Assignments/Assignment2.ts
@@ -0,0 +1,10 @@
+// if else condition
+let cusName:string = '"Mr. John';
+let creditScore:number = 750;
+if(creditScore > 750){
+    console.log(+cusName, "you are eligible for loan: ");
+
+    if(creditScore < 750){
+console.log(+cusName, "You are not eligible for loan:")
+    }
+}
